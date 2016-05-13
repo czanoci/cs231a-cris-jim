6e88d7c6a828587daf9a5be3f630b8d5f97e55c9
Jim <jandress@stanford.edu>
2016-05-13 04:35:15 -0700

    Debugged and tested the edge matching code.  Preliminary result was 60% of edges matched with their true neighbor.

--- edge_scores.py
+++ edge_scores.py
@@ -6,39 +6,43 @@ from image import *
 import matplotlib.pyplot as plt
 
 def get_nonsym_compat(G, mu, S):
-	sum = 0
+	compat = 0
 	G -= mu
-	S_inv = np.inv(S)
-	for p in xrange(P):
-		sum += G[p, :] * S_inv * np.transpose(G[p, :])
-	return sum
+	S_inv = np.linalg.inv(S)
+	return np.sum(np.dot(G, S_inv) * G)
 
 def get_compat(s1, s2, r1, r2):
 	p1 = s1.get_rotated_pixels(r1)
 	p2 = s2.get_rotated_pixels(r2)
 	G = p2[:, 0, :] - p1[:, P - 1, :]
-	sum = get_nonsym_compat(G, s1.get_left_mean(r1), s1.get_left_covar(r1)) + \
+	compat = get_nonsym_compat(G, s1.get_left_mean(r1), s1.get_left_covar(r1)) + \
 		  get_nonsym_compat(-G, s2.get_right_mean(r2), s2.get_right_covar(r2))
-	return sum
+	return compat
 
 
 img = Image(img_filename)
 scramble(img)
+for sq in img.pieces:
+	sq.compute_mean_and_covar()
 
 K = len(img.pieces)
 
 dists = np.zeros([K, 4, K, 4])
 
+print K
 for i in xrange(K):
+	print i
 	for ri in xrange(4):
-		for j in xrange(K):
+		for j in range(i+1, K):
 			for rj in xrange(4):
 				dists[i, ri, j, rj] = get_compat(img.pieces[i], img.pieces[j], ri, rj)
+				dists[j, (ri + 2)%4, i, (rj + 2)%4] = dists[i, ri, j, rj]
 
 
 correct_pieces = 0
 correct_rots = 0
 for i in xrange(K):
+	print i
 	square = img.pieces[i]
 	for ri in xrange(4):
 		min_dist = float("inf");
