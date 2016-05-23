Hopefully fixed the bug so that the mismatch pictures for SSD will work. We had an overflow which meant that really bad matches were counting as the closest, since their distance was so large it overflowed to negative.

image.py
@@ -83,7 +83,7 @@ def scramble(img, type2=True):
 	for i in xrange(num_squares_H):
 		for j in xrange(num_squares_W):
 			pixels = img.reduced[i*P:(i+1)*P, j*P:(j+1)*P, :]
-			pixels = pixels.astype(np.int16)
+			pixels = pixels.astype(np.int32)
 			rot_idx = 0
 			if type2:
 				rot_idx = random.randint(0, 3)

test.py
@@ -30,10 +30,9 @@ print correct_rots_list
 
 print np.mean(correct_pieces_list)
 print np.mean(correct_rots_list)
-
 '''
 
-
+random.seed(1122334455667)
 img = Image(img_filename)
 scramble(img, type2)
 
@@ -42,6 +41,13 @@ for sq in img.pieces:
 
 dists_mgc = compute_edge_dist(img, type2)
 dists_ssd = compute_edge_dist(img, type2, ssd=True)
+np.save('Data/dists_mgc', dists_mgc)
+np.save('Data/dists_ssd', dists_ssd)
+
+'''
+dists_mgc = np.load('Data/dists_mgc.npy')
+dists_ssd = np.load('Data/dists_ssd.npy')
+'''
 
 K = len(img.pieces)
 correct_pieces = 0