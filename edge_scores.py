Fixed bug for SSD and made the changes so that the correct edge percent computation only includes real edges.  We now get numbers comparable to the paper.

edge_scores.py
@@ -5,16 +5,16 @@ from constants import *
 from image import *
 import matplotlib.pyplot as plt
 
-def get_nonsym_compat(G, mu, S):
+def get_nonsym_compat(G, mu, S_inv):
 	compat = 0
 	G -= mu
-	S_inv = np.linalg.inv(S)
+	#S_inv = np.linalg.inv(S)
 	return np.sum(np.dot(G, S_inv) * G)
 
-def get_compat(left_mean, left_covar, right_mean, right_covar, p_i, p_j):
+def get_compat(left_mean, left_covar_inv, right_mean, right_covar_inv, p_i, p_j):
 	G = p_j[:, 0, :] - p_i[:, P - 1, :]
-	compat = get_nonsym_compat(G, left_mean, left_covar) + \
-		  get_nonsym_compat(-G, right_mean, right_covar)
+	compat = get_nonsym_compat(G, left_mean, left_covar_inv) + \
+		  get_nonsym_compat(-G, right_mean, right_covar_inv)
 	return compat
 
 def get_ssd_compat(p_i, p_j):
@@ -46,7 +46,7 @@ def compute_edge_dist(img, type2=True, ssd=False):
 		for ri in xrange(4):
 			p_i = sq_i.get_rotated_pixels(ri)
 			left_mean = sq_i.get_left_mean(ri)
-			left_covar = sq_i.get_left_covar(ri)
+			left_covar_inv = sq_i.get_left_covar_inv(ri)
 			for j in range(i+1, K):
 				sq_j = img.pieces[j]
 				for rj in xrange(4):
@@ -55,11 +55,14 @@ def compute_edge_dist(img, type2=True, ssd=False):
 						continue
 
 					p_j = sq_j.get_rotated_pixels(rj)
-					right_mean = sq_j.get_right_mean(rj)
-					right_covar = sq_j.get_right_covar(rj)
 
-					dists[i, ri, j, rj] = get_compat(left_mean, left_covar, right_mean, right_covar, p_i, p_j) \
-											if not ssd else get_ssd_compat(p_i, p_j) 
+					if not ssd:
+						right_mean = sq_j.get_right_mean(rj)
+						right_covar_inv = sq_j.get_right_covar_inv(rj)
+						dists[i, ri, j, rj] = get_compat(left_mean, left_covar_inv, right_mean, right_covar_inv, p_i, p_j)
+					else:
+						dists[i, ri, j, rj] = get_ssd_compat(p_i, p_j)
+
 					dists[j, (rj + 2)%4, i, (ri + 2)%4] = dists[i, ri, j, rj]
 	return dists
 
@@ -94,7 +97,7 @@ def count_correct_matches(img, dists, type2=True):
 				if cor_rot:
 					correct_rots += 1
 					
-	return correct_pieces/total_neighbors, correct_rots/total_neighbors
+	return correct_pieces/(1.0*total_neighbors), correct_rots/(1.0*total_neighbors)
 
 def get_nearest_edge(i, ri, dists, K, type2=True):
 	min_dist = float("inf");

image.py
@@ -36,14 +36,14 @@ class Square:
 		self.pix = pixels
 
 		self.mean = np.zeros(4)
-		self.covar = np.zeros(4)
+		self.covar_inv = np.zeros(4)
 
 	def get_rotated_pixels(self, rot=0):
 		return np.rot90(self.pix, rot)
 
-	def compute_mean_and_covar(self):
+	def compute_mean_and_covar_inv(self):
 		means = []
-		covars = []
+		covars_inv = []
 
 		dummy_grad = np.array([ [0, 0, 0], [1, 1, 1], [-1, -1, -1], [0, 0, 1], [0, 1, 0], [1, 0, 0], [-1, 0, 0], [0, -1, 0], [0, 0, -1] ])
 
@@ -52,13 +52,13 @@ class Square:
 			GL = pixels[:, P-1, :] - pixels[:, P-2, :]
 			mu = np.mean(GL, axis=0)
 			GL_plus_dummy = np.concatenate((GL, dummy_grad))
-			cov = np.cov(np.transpose(GL_plus_dummy))
+			cov_inv = np.linalg.inv(np.cov(np.transpose(GL_plus_dummy)))
 
 			means.append(mu)
-			covars.append(cov)
+			covars_inv.append(cov_inv)
 
 		self.mean = np.array(means)
-		self.covar = np.array(covars)
+		self.covar_inv = np.array(covars_inv)
 
 	def get_left_mean(self, rot=0):
 		return self.mean[rot]
@@ -66,11 +66,11 @@ class Square:
 	def get_right_mean(self, rot=0):
 		return self.mean[(rot+2)%4]
 
-	def get_left_covar(self, rot=0):
-		return self.covar[rot]
+	def get_left_covar_inv(self, rot=0):
+		return self.covar_inv[rot]
 
-	def get_right_covar(self, rot=0):
-		return self.covar[(rot+2)%4]
+	def get_right_covar_inv(self, rot=0):
+		return self.covar_inv[(rot+2)%4]
 
 
 

test.py
@@ -10,7 +10,7 @@ from edge_scores import *
 type2 = True
 ssd = False
 
-'''
+
 correct_pieces_list = []
 correct_rots_list = []
 
@@ -19,17 +19,17 @@ for f in os.listdir(img_folder):
 	img = Image(img_folder + f)
 	scramble(img, type2)
 	for sq in img.pieces:
-		sq.compute_mean_and_covar()
+		sq.compute_mean_and_covar_inv()
 	dists = compute_edge_dist(img, type2, ssd)
 	correct_pieces, correct_rots = count_correct_matches(img, dists, type2)
 	correct_pieces_list.append(correct_pieces)
 	correct_rots_list.append(correct_rots)
-
-print correct_pieces_list
-print correct_rots_list
+	print correct_pieces_list
+	print correct_rots_list
 
 print np.mean(correct_pieces_list)
 print np.mean(correct_rots_list)
+
 '''
 
 random.seed(1122334455667)
@@ -37,17 +37,17 @@ img = Image(img_filename)
 scramble(img, type2)
 
 for sq in img.pieces:
-	sq.compute_mean_and_covar()
+	sq.compute_mean_and_covar_inv()
 
 dists_mgc = compute_edge_dist(img, type2)
 dists_ssd = compute_edge_dist(img, type2, ssd=True)
 np.save('Data/dists_mgc', dists_mgc)
 np.save('Data/dists_ssd', dists_ssd)
 
-'''
+
 dists_mgc = np.load('Data/dists_mgc.npy')
 dists_ssd = np.load('Data/dists_ssd.npy')
-'''
+
 
 K = len(img.pieces)
 correct_pieces = 0
@@ -82,7 +82,7 @@ for i in xrange(K):
 			'./Images/differences/diff' + str(count) + '.jpg')
 		count += 1
 
-'''
+
 #img = Image(img_filename)
 #scramble(img)
 #assemble_image(img)